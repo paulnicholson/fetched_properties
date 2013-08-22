9e54b1e3fdccc2b59d1dc6096b7398d404fda108
Paul Nicholson <paul@refactoredtech.com>
2013-08-22 10:55:30 -0400

    Simple fetched properties example

--- Fetched Properties/MasterViewController.m
+++ Fetched Properties/MasterViewController.m
@@ -39,16 +39,18 @@
 
 - (void)insertNewObject:(id)sender
 {
+    NSDate *date = [NSDate date];
     NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
-    NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
-    NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
-    
-    // If appropriate, configure the new managed object.
-    // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-    [newManagedObject setValue:[NSDate date] forKey:@"timeStamp"];
+
+    NSManagedObject *newEventObject = [NSEntityDescription insertNewObjectForEntityForName:@"Event" inManagedObjectContext:context];
+    [newEventObject setValue:@([date timeIntervalSince1970]) forKey:@"entity_id"];
     
     // Save the context.
     NSError *error = nil;
+    NSManagedObject *newTimestampObject = [NSEntityDescription insertNewObjectForEntityForName:@"Timestamp" inManagedObjectContext:context];
+    [newTimestampObject setValue:date forKey:@"timestamp"];
+    [newTimestampObject setValue:@([date timeIntervalSince1970]) forKey:@"event_id"];
+
     if (![context save:&error]) {
          // Replace this implementation with code to handle the error appropriately.
          // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development. 
@@ -131,7 +133,7 @@
     [fetchRequest setFetchBatchSize:20];
     
     // Edit the sort key as appropriate.
-    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"timeStamp" ascending:NO];
+    NSSortDescriptor *sortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"entity_id" ascending:NO];
     NSArray *sortDescriptors = @[sortDescriptor];
     
     [fetchRequest setSortDescriptors:sortDescriptors];
@@ -216,7 +218,7 @@
 - (void)configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath
 {
     NSManagedObject *object = [self.fetchedResultsController objectAtIndexPath:indexPath];
-    cell.textLabel.text = [[object valueForKey:@"timeStamp"] description];
+    cell.textLabel.text = [[[[object valueForKey:@"timestamps"] firstObject] valueForKey:@"timestamp"]description];
 }
 
 @end
